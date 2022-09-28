657a1229a30f5d378c97ae124ae1eac76d6a01ca
raykevinconcha <tudela2352>
2022-09-27 21:05:11 -0500

    borrar y ver mascota

--- src/main/java/com/example/lab4_gtics/controller/ContactoController.java
+++ src/main/java/com/example/lab4_gtics/controller/ContactoController.java
@@ -1,16 +1,19 @@
 package com.example.lab4_gtics.controller;
 
 import com.example.lab4_gtics.entity.Cuenta;
+import com.example.lab4_gtics.entity.Mascota;
 import com.example.lab4_gtics.repository.CuentaRepository;
 import com.example.lab4_gtics.repository.MascotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,26 +30,46 @@ public class ContactoController {
 
     @GetMapping("/nuevo")
     public String nuevoContacto() {
-        return "contacto/nuevo";
+        return "contacto/newFrm";
     }
 
     @PostMapping("/guardar")
     public String guardarContacto(Cuenta cuenta, RedirectAttributes attr){
         attr.addFlashAttribute("msg1", "Contacto guardado exitosamente");
         cuentaRepository.save(cuenta);
-        return "redirect:/contacto/list";
+        return "redirect:/contacto/listaduenos";
 
     }
     @GetMapping("/borrar")
-    public String borrarContacto(@RequestParam("id") Integer id, RedirectAttributes attr){
+    public String borrarContacto(@RequestParam("id") Integer id, RedirectAttributes attr) {
         Optional<Cuenta> oCuenta = cuentaRepository.findById(id);
-        if(oCuenta.isPresent()) {
+        if (oCuenta.isPresent()) {
             attr.addFlashAttribute("msg3", "Contacto borrado exitosamente");
             cuentaRepository.delete(oCuenta.get());
         }
-        return "redirect:/contacto/list";
+        return "redirect:/contacto/listaduenos";
 
+    }
+    @GetMapping("/borrarMascota")
+    public String borrarMascota(@RequestParam("id") Integer id,RedirectAttributes attr){
+        Optional<Mascota> oMascota = mascotaRepository.findById(id);
+        if(oMascota.isPresent()) {
+            mascotaRepository.delete(oMascota.get());
+            attr.addFlashAttribute("msg4", "Mascota borrada exitosamente");
+        }
+        return "redirect:/contacto/listamascotas";
 
+    }
+
+    @GetMapping("/verMascota")
+    public String verMascota(@RequestParam("id") int id, Model model) {
+
+        List<Mascota> mascotas = mascotaRepository.obtenerListaMascotas(id);
+        model.addAttribute("mascotas", mascotas);
+
+        return "contacto/listamascotas";
+
+    }
 
 
 }
