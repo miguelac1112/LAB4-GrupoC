da4ee0063e91ef823c85026029848ce0b2a7c571
raykevinconcha <tudela2352>
2022-09-27 20:45:58 -0500

    1er

--- src/main/java/com/example/lab4_gtics/controller/ContactoController.java
+++ src/main/java/com/example/lab4_gtics/controller/ContactoController.java
@@ -1,4 +1,18 @@
 package com.example.lab4_gtics.controller;
 
+import com.example.lab4_gtics.repository.CuentaRepository;
+import com.example.lab4_gtics.repository.MascotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("contactos")
 public class ContactoController {
+    @Autowired
+    CuentaRepository cuentaRepository;
+
+    @Autowired
+    MascotaRepository mascotaRepository;
+
 }
